e14f455af9964c669ca00cbe2b0cc68f4f010b93
Linnik Ilya <linnik.i@market-soft.ru>
2024-12-12 01:32:08 +0300

    1 version

---
+++ utils.go
@@ -0,0 +1,21 @@
+//файл utils.go
+
+package utils
+
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
